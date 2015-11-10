90c8252707ef1233be819f633498d0c1e3effcd1
makaravind <makaravind11195@gmail.com>
2015-11-10 08:59:38 +0100

    Submit Commit

--- src/scoresDescendingSort.cpp
+++ src/scoresDescendingSort.cpp
@@ -19,7 +19,50 @@ struct student {
 	char name[10];
 	int score;
 };
+int partition(student *A, int p, int r) {
+	int x = A[p].score; // pivot
+	int i = p;
+	int j = r;
+	while (true) {
 
+		while (A[i].score > x) {
+			i++;
+		}
+
+		while (A[j].score < x) {
+			j--;
+		}
+		if (i < j) {
+			int temp = A[i].score;
+			A[i].score = A[j].score;
+			A[j].score = temp;
+		}
+		else {
+			return j;
+		}
+	}
+}
+void quicksort(student *A, int p, int r) {
+	if (p < r) {
+		int q = partition(A, p, r);
+		quicksort(A, p, q);
+		quicksort(A, q + 1, r);
+	}
+}
+int IfAllSame(struct student *students, int len){
+	//Same elements checking
+	int ref = students[0].score;
+	int flag = -1;
+	for (int i = 1; i < len; i++)
+		if (students[i].score != ref) return 1;
+	return 0;
+}
 void * scoresDescendingSort(struct student *students, int len) {
-	return NULL;
+	// init checking 
+	if (students == NULL) return NULL;
+	if (len < 0) return NULL;
+
+	if(IfAllSame(students,len)) 
+		quicksort(students, 0, len - 1);
+	return students;
 }
