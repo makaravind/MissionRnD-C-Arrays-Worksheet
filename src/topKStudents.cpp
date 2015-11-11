2962f78770e29f19d4b290364b1c3e0340961ce3
makaravind <makaravind11195@gmail.com>
2015-11-11 08:26:37 +0100

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -15,12 +15,84 @@ NOTES:
 
 #include <iostream>
 #include <malloc.h>
+#include<limits.h>
 
 struct student {
 	char *name;
 	int score;
 };
+int partition1(student *A, int p, int r) {
+	int x = A[p].score; // pivot
+	int i = p;
+	int j = r;
+	while (true) {
 
+		while (A[i].score > x) {
+			i++;
+		}
+
+		while (A[j].score  < x) {
+			j--;
+		}
+		if (i < j) {
+			int temp = A[i].score;
+			A[i].score = A[j].score;
+			A[j].score = temp;
+		}
+		else {
+			return j;
+		}
+	}
+}
+void quicksort1(student *A, int p, int r) {
+	if (p < r) {
+		int q = partition1(A, p, r);
+		quicksort1(A, p, q);
+		quicksort1(A, q + 1, r);
+	}
+}
 struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	if (students == NULL) return NULL;
+	if (len < 0) return NULL;
+	if (K <= 0) return NULL;
+
+	//creating return pointer
+	struct student **top;
+	top = (struct student **) malloc(K * sizeof(struct student));
+	for (int i = 0; i < K; i++)
+		top[i] = (struct student *)malloc(sizeof(struct student) * 1);
+
+	// if K greater than len
+	if (K > len) {
+		for (int i = 0; i < len; i++)
+			top[i] = &students[i];
+		return top;
+	}
+
+	int max;
+	int maxid;
+	int ref = INT_MAX;
+	//int result[100];
+
+	for (int j = 0; j < K; j++)
+	{
+	max = INT_MIN;
+	for ( int i = 0; i < len; i++){
+	if (students[i].score > max && students[i].score < ref) {
+	max = students[i].score;
+	maxid = i;
+	}
+
+	}
+	ref = max;
+	top[j] = &students[maxid];
+	//result[j] = max;
+	}
+
+	/*quicksort1(students, 0, len - 1);
+
+	for (int i = len - 1; i <= K; i++){
+		top[i] = &students[i];
+	}*/
+	return top;
 }
